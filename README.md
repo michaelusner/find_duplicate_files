73ad3bdfca7c9a111d3cb255c4a21cd71cd0e3bf
Michael Usner <michael.usner@hpe.com>
2017-10-27 22:54:12 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This simple go program will find duplicate files on your disk.
+usage:
+go run find_duplicate_files.go  <folder>
+
+You will be prompted which duplicate file to keep.
---
+++ find_duplicate_files.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+    "github.com/minio/sha256-simd"
+    "path/filepath"
+    "os"
+    "log"
+    "io"
+    "flag"
+    "fmt"
+    "encoding/hex"
+)
+
+type FileInfo struct {
+    path string
+    hash string
+}
+
+var files map[int64][]string
+
+func GetHash(path string) string {
+    file, err := os.Open(path)
+    if (err != nil) {
+        log.Printf("Failed to open %s", path)
+    }
+    defer file.Close()
+    shaWriter := sha256.New()
+    io.Copy(shaWriter, file)
+    return hex.EncodeToString(shaWriter.Sum(nil))
+}
+
+func visit(path string, f os.FileInfo, err error) error {
+    if (!f.IsDir()) {
+        files[f.Size()] = append(files[f.Size()], path)
+    } else {
+        fmt.Printf("%s\n", path)
+    }
+    return nil
+} 
+
+
+func main() {
+    files = make(map[int64][]string)
+        
+    flag.Parse()
+    root := flag.Arg(0)
+    err := filepath.Walk(root, visit)
+    if (err != nil) {
+        fmt.Print("Failed to walk path")
+    }
+
+    for _, paths := range files {
+        if (len(paths) > 1) {
+            hashes := make(map[string]string)
+            for _, path := range(paths) {
+                //fmt.Println(path)
+                hash := GetHash(path)
+                if val, ok := hashes[hash]; ok {
+                    fmt.Println("a: ", val)
+                    fmt.Println("b: ", path)
+                    fmt.Println("Keep A or B")
+                    var input string
+                    fmt.Scanln(&input)
+                    if (input == "a") {
+                        fmt.Println("Keeping a")
+                        os.Remove(path)
+                    } else if (input == "b") {
+                        fmt.Println("Keeping b")
+                        os.Remove(val)
+                    }
+                } else {
+                    hashes[hash] = path
+                }
+            }
+            
+            fmt.Println()
+        }
+    }
+
+}
